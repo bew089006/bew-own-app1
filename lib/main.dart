638c523e9f040f45329339bef76c21e38aaf23e3
wattanan <bew08900@gmail.com>
2020-12-05 17:26:44 +0700

    completed bew-own-app1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
@@ -20,8 +21,25 @@ class QuestionPage extends StatefulWidget {
 }
 
 class _QuestionPageState extends State<QuestionPage> {
+  int index = 1;
+
+  void randomIndex() {
+    setState(() {
+      index = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Image.asset('images/ball1.png');
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            randomIndex();
+          });
+        },
+        child: Image.asset('images/ball$index.png'),
+      ),
+    );
   }
 }
