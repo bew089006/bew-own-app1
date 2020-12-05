7bc6d935132b97b1c5442f38e0b45a157f51ebc2
wattanan <bew08900@gmail.com>
2020-12-05 17:16:08 +0700

    initail picture

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Bew Won App 1'),
+        ),
+        body: QuestionPage(),
+        backgroundColor: Colors.blue,
       ),
-    );
+    ),
+  );
+}
+
+class QuestionPage extends StatefulWidget {
+  @override
+  _QuestionPageState createState() => _QuestionPageState();
+}
+
+class _QuestionPageState extends State<QuestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('images/ball1.png');
+  }
+}
